a7fafd6989f06c9585a00d65be956fcd0ecb78f2
xDamneDx <xdamnedx@gmail.com>
2022-06-27 14:19:59 +0300

    Created register & login functions

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,12 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
+  user: "null",
   isError: false,
   isSuccess: false,
   isLoading: false,
   message: "",
 };
+
+// Register new user:
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
+
+// Login user:
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
 
 export const authSlice = createSlice({
   name: "auth",
