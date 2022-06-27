b8bf997ce3e5c026878ed505715fd11f03ee2821
xDamneDx <xdamnedx@gmail.com>
2022-06-27 10:33:41 +0300

    Added pages, component & routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages:
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+// Components:
+import Header from "./components/Header";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
