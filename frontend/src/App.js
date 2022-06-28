fa4f6b5df1c78ddce385ec9b057fa12ff8a835e8
xDamneDx <xdamnedx@gmail.com>
2022-06-28 14:58:42 +0300

    Added NewTicket, PrivateRoute & routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,11 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NewTicket from "./pages/NewTicket";
 
 // Components:
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
