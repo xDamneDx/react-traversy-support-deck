56eacc4d80cb6cbbb00d2c7401bed16dde171471
xDamneDx <xdamnedx@gmail.com>
2022-06-28 11:22:19 +0300

    Added getting single ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,33 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc Get user ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using the id ine thw JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
@@ -49,4 +76,4 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
-module.exports = { getTickets, createTicket };
+module.exports = { getTickets, getTicket, createTicket };
