0f29ba5e60f6f848296004d1fd05a325a56e1a7a
xDamneDx <xdamnedx@gmail.com>
2022-06-29 14:34:51 +0300

    Added TicketItem component

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -5,6 +5,7 @@ import { getTickets, reset } from "../features/tickets/ticketSlice";
 // Components:
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 function Tickets() {
   const { tickets, isLoading, isSuccess } = useSelector(
@@ -26,7 +27,23 @@ function Tickets() {
     return <Spinner />;
   }
 
-  return <div>Tickets</div>;
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Tickets;
